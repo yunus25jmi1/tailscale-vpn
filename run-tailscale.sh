b42411a4dc42ad90ca7d247f48b1adfbf1d3b10d
MD YUNUS <115855149+yunus25jmi1@users.noreply.github.com>
2024-10-09 21:14:47 +0530

    Update run-tailscale.sh

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -7,7 +7,7 @@ ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
 until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
   sleep 0.1
 done
-export ALL_PROXY=socks5://localhost:1055/
+# export ALL_PROXY=socks5://localhost:1055/
 tailscale_ip=$(/render/tailscale ip)
 echo "Tailscale is up at IP ${tailscale_ip}"
 
